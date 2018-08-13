2f085f144dfbbc0faae9c6a85479e19cf6d7e131
Dave Scherler <davescherler@Daves-MBP-Work.localdomain>
2018-08-13 14:24:38 -0400

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Dave Scherler Portfolio',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi, I'm Dave Scherler</h1>
+    <p>I'm a user experience designer living in Washington, DC.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
